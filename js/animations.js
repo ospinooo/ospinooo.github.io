9400269580008dfe384f68f290d1471fd80a2f29
Ospino <pablo.ospino@nispera.com>
2021-02-26 09:41:20 +0100

    add javascript animation with jQuery onscroll
    Signed-off-by: Ospino <pablo.ospino@nispera.com>

---
+++ js/animations.js
@@ -0,0 +1,14 @@
+$(document).on("scroll", function() {
+  var pageTop = $(document).scrollTop();
+  var pageBottom = pageTop + $(window).height();
+  var tags = $("section");
+  
+  for (var i = 0; i < tags.length; i++) {
+    var tag = tags[i];
+    
+    if ($(tag).position().top < pageBottom) {
+      $(tag).addClass("fade-in");
+    } 
+  }
+});
+  
